2a3c3b2e7c7796bb37b050a092062e138038ef58
thofma <thofma@gmail.com>
2018-02-12 19:34:02 +0100

    Integrate

--- src/arb/acb_calc.jl
+++ src/arb/acb_calc.jl
@@ -14,6 +14,38 @@ acb_calc_func_wrap_c() = cfunction(acb_calc_func_wrap, Cint,
 const ARB_CALC_SUCCESS = UInt(0)
 const ARB_CALC_NO_CONVERGENCE = UInt(2)
 
+function _integrate(F, a, b, abs_tol::Int = 53)
+    opts = acb_calc_integrate_opts()
+    _abs_tol = mag_set_ui_2exp_si(mag_struct(), 1, -abs_tol)
+    rel_goal = abs_tol
+
+    res = AcbField(abs_tol)()
+
+    g = function(z, x, para, order, prec)
+      xx = unsafe_load(x)
+      xx.parent = AcbField(prec)
+      w = F(xx)
+      ccall((:acb_set, :libarb), Ptr{Void}, (Ptr{acb}, Ref{acb}), z, w)
+      return zero(Cint)
+    end
+
+    gptr = cfunction(g, Cint, (Ptr{acb}, Ptr{acb}, Ptr{Void}, Int, Int))
+
+    status = ccall((:acb_calc_integrate, :libarb), UInt,
+        (Ref{acb},   #res
+        Ptr{Void},   #func
+        Ptr{Void},   #params
+        Ref{acb},    #a
+        Ref{acb},    #b
+        Int,         #rel_goal
+        Ref{mag_struct},    #abs_tol
+        Ref{acb_calc_integrate_opts}, #opts
+        Int),
+        res, gptr, C_NULL, a, b, rel_goal, _abs_tol, opts, abs_tol)
+
+    return res
+end
+
 function integrate(F::Function, a::acb, b::acb;
     rel_goal::Int=prec(parent(a)),
     abs_tol::mag_struct=mag_set_ui_2exp_si(mag_struct(), 1, -prec(parent(a))),
